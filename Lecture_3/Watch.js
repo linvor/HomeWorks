5dd4ff57ac43326b91cdcab2333889269bb086d0
linvor <51704539+linvor@users.noreply.github.com>
2019-06-20 15:22:31 +0300

    Add files via upload

---
+++ Lecture_3/Watch.js
@@ -0,0 +1,43 @@
+class Watch {
+  constructor() {
+    this.interval = null;
+  }
+
+  start() {
+    this.interval = setInterval(function() {
+
+      let date = new Date();
+      //console.log(date.toLocaleTimeString());
+      let hours = date.getHours();
+      let min = date.getMinutes();
+      let sec = date.getSeconds();
+
+        if(hours < 10)
+          hours = '0' + hours;
+        if(min < 10)
+          min = '0' + min;
+        if(sec < 10)
+          sec = '0' + sec;
+
+      let time = hours + ':' + min + ':' + sec;
+
+      console.log(time);
+
+    }, 2000);  
+  }
+
+  stop() {
+    clearInterval(this.interval);
+    console.clear();
+  }
+}
+
+const watch = new Watch();
+
+watch.start();
+
+
+setTimeout(() => {
+  // Остановится через 10 секунд
+  watch.stop();
+}, 10000);
