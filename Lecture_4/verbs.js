71dddf9ac78614ca0d0e8f3fcb62c6cc72e5fd38
linvor <51704539+linvor@users.noreply.github.com>
2019-06-25 14:58:47 +0300

    Add files via upload

---
+++ Lecture_4/verbs.js
@@ -0,0 +1,28 @@
+class Speaker {
+  constructor(name, verb) {
+    this.name = name;
+    //this.verb = verb;
+  }
+
+  set verb(verbal) {
+    this.verbal = verbal;
+  }
+  get verb() {
+    return this.verbal || "says";
+  }
+
+  speak(text) {
+    console.log(this.name + " " + this.verb + " '" + text + "'");
+  }
+}
+
+class Shouter extends Speaker {
+  constructor(name) {
+    super(name);
+  }
+  speak(text) {
+    super.speak.call(this, text.toUpperCase());
+  }
+}
+
+new Shouter("Dr. Loudmouth").speak("hello there");
