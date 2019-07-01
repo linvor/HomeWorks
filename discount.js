6698aed330f0b7b648c69f609e70d67dfc90ccae
linvor <51704539+linvor@users.noreply.github.com>
2019-07-01 15:43:55 +0300

    Delete discount.js

--- discount.js
+++
@@ -1,8 +0,0 @@
-const makeDiscount = (percent) => {
-  const sum = (price) => price - price / 100 * percent;
-  return sum;
-}
-
-console.log(makeDiscount(10)(100));
-console.log(makeDiscount(90)(100));
-
